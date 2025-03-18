43acdfba28d3d3426f8ae6bb9728b03bb1a14b3a
Edriel0w0 <129696322+Edriel0w0@users.noreply.github.com>
2025-03-18 17:48:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ function agregarAmigo() {
 
     amigos.push(nombre);
     
-    // Crear un nuevo elemento de lista y agregarlo al DOM
+
     const nuevoElemento = document.createElement("li");
     nuevoElemento.textContent = nombre;
     listaAmigos.appendChild(nuevoElemento);
     
-    // Limpiar el campo de entrada
+
     input.value = "";
 }
 
@@ -31,11 +31,11 @@ function sortearAmigo() {
     const indiceAleatorio = Math.floor(Math.random() * amigos.length);
     const amigoSeleccionado = amigos[indiceAleatorio];
     
-    // Mostrar el resultado en la lista de resultado
+
     const resultadoLista = document.getElementById("resultado");
     resultadoLista.innerHTML = `<li>El amigo secreto sorteado es: <strong>${amigoSeleccionado}</strong></li>`;
     
-    // Limpiar la lista de amigos visualmente y vaciar el array
+
     document.getElementById("listaAmigos").innerHTML = "";
     amigos = [];
 }
